e4ef5979e2b752a0a46d48814109cd08dc01df76
TontoKM <tonto3975@gmail.com>
2026-04-06 18:55:51 +0900

    NodeJS 환경 구축

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Together 서버 작동중!');
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
