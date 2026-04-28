469bbceb1b8d58c1b2cd33a1839cf663d9b1ca2c
JoYeonO <oh2972@gmail.com>
2026-04-28 19:59:08 +0900

    settings 추가

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
