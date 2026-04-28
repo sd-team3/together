ccba919c1ea015968b4b5a1b7e31545b697fe779
이윤재 <20201459@daejin.ac.kr>
2026-04-28 20:58:06 +0900

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,34 @@
 const userService = require('../services/userService');
 
 //# 회원 가입 페이지
+<<<<<<< Updated upstream
 const getSignup = (req, res) => {
     res.render('user/signup', {
+=======
+const getJoin = (req, res) => {
+    res.render('user/join', {
+>>>>>>> Stashed changes
         errors: {}
     });
 };
 
 //# 회원 가입 처리
+<<<<<<< Updated upstream
 const postSignup = async (req, res, next) => {
+=======
+const postJoin = async (req, res, next) => {
+>>>>>>> Stashed changes
     try {
         const { email, password, name, address } = req.body;
         //회원 생성
         await userService.createUser({ email, password, name, address, uploadFile: req.file });
         res.redirect('/');
     } catch (error) {
+<<<<<<< Updated upstream
         return next(error);
+=======
+        return next(error);//에러를 미들웨어에게 넘기기
+>>>>>>> Stashed changes
     }
 };
 
@@ -38,6 +51,7 @@ const logout = (req, res, next) => {
 }
 
 //마이페이지
+<<<<<<< Updated upstream
 const getProfile = async (req, res) => {
     if (!req.isAuthenticated()) {
         return res.redirect('/user/login');
@@ -55,6 +69,25 @@ const getEditProfile = async (req, res) => {
 
 //회원 수정 처리
 const postEditProfile = async (req, res, next) => {
+=======
+const getInfo = async (req, res) => {
+    if (!req.isAuthenticated()) {
+        return res.redirect('/user/login');
+    }//인증되지 않은 사용자가 마이페이지 요청 시 로그인 페이지로 이동
+    res.render('user/info');
+};
+
+//회원 수정 페이지
+const getModify = async (req, res) => {
+    if (!req.isAuthenticated()) {
+        return res.redirect('/user/login');
+    }//인증되지 않은 사용자가 회원수정 페이지 요청 시 로그인 페이지로 이동
+    res.render('user/modify');
+}
+
+//회원 수정 처리
+const postModify = async (req, res, next) => {
+>>>>>>> Stashed changes
     if (!req.isAuthenticated()) {
         return res.redirect('/user/login');
     }
@@ -62,8 +95,13 @@ const postEditProfile = async (req, res, next) => {
     const { password, name, address } = req.body;
     try {
         await userService.updateUser(req.user.id, { password, name, address, uploadFile: req.file});
+<<<<<<< Updated upstream
         //
         res.redirect('/user/profile');
+=======
+        //DB에서 회원정보 수정(세션에 들어있는 회원의 기본키와 수정하고자 할 값 들)
+        res.redirect('/user/info');
+>>>>>>> Stashed changes
         //수정 후 마이페이지로 이동
 
     } catch (error) {
@@ -97,13 +135,23 @@ const postDelete = async (req, res, next) => {
 const checkEmail = async (req, res, next) => {
     const { email } = req.query;
     try {
+<<<<<<< Updated upstream
         const available = await userService.checkEmail(email);
         console.log(available);
         res.json({ available });//DB에 해당 email이 있으면 false, 없으면 true
+=======
+        const avalilabe = await userService.checkEmail(email);
+        console.log(avalilabe);
+        res.json({ avalilabe });//DB에 해당 email이 있으면 false, 없으면 true
+>>>>>>> Stashed changes
     } catch (error) {
         next(error);
     }
 
 }
 
+<<<<<<< Updated upstream
 module.exports = { getSignup, postSignup, getLogin, logout, getProfile, getEditProfile, postEditProfile, getDelete, postDelete, checkEmail };
+=======
+module.exports = { getJoin, postJoin, getLogin, logout, getInfo, getModify, postModify, getDelete, postDelete, checkEmail };
+>>>>>>> Stashed changes
--- services/userService.js
+++ services/userService.js
@@ -22,15 +22,25 @@ async function createUser({ email, password, name, address, uploadFile }) {
 
     //DB 저장
     await newUser.save();
+<<<<<<< Updated upstream
     return newUser;
 
 }
 //email로 특정 회원 가져오기
+=======
+
+}
+//email로 특정 회원 가져오기(passport 로그인 시 사용)
+>>>>>>> Stashed changes
 const findUserByEmail = async (email) => {
     return await User.findOne({ email });
 }
 
+<<<<<<< Updated upstream
 //ID(고유값)로 특정 회원 가져오기
+=======
+//ID(고유값)로 특정 회원 가져오기(passport 인증 시 사용)
+>>>>>>> Stashed changes
 const findUserById = async (id) => {
     return await User.findById(id);
 }
@@ -48,12 +58,20 @@ async function updateUser(userId, { password, name, address, uploadFile }) {
     if (!user) {
         throw new Error('사용자를 찾을 수 없습니다');
     }
+<<<<<<< Updated upstream
    
+=======
+    //const hashedPassword = await bcrypt.hash(password, 10);
+>>>>>>> Stashed changes
 
     user.name = name;
     user.address = address;
     user.profileImage = profile;
+<<<<<<< Updated upstream
    
+=======
+    //user.password = hashedPassword;
+>>>>>>> Stashed changes
 
     await user.save();
 }
@@ -69,7 +87,11 @@ async function deleteUser(userId, password) {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
         const error = new Error('비밀번호가 일치하지 않습니다');
+<<<<<<< Updated upstream
         error.status = 400;
+=======
+        error.status = 400;//Bad Request: 클라이언트의 잘못된 요청
+>>>>>>> Stashed changes
         throw error;
     }
     if (!user) {
@@ -81,7 +103,11 @@ async function deleteUser(userId, password) {
 async function checkEmail(email) {
     console.log(email);
     const user = await User.findOne({ email });
+<<<<<<< Updated upstream
     return !user;
+=======
+    return !user;//DB에 해당 email이 있으면 true
+>>>>>>> Stashed changes
 }
 
 // 소셜 회원 DB 저장
@@ -98,4 +124,8 @@ async function createSocialUser({email, name, profileImage, address, provider}) 
 }
 
 module.exports = { createUser, findUserByEmail, 
+<<<<<<< Updated upstream
     findUserById, updateUser, deleteUser, checkEmail, createSocialUser };
+=======
+    findUserById, updateUser, deleteUser, checkEmail, createSocialUser };
+>>>>>>> Stashed changes
