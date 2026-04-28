698f7dfc4b46f271232c07b5c8a43ed2522b8c73
yooseunghwan <shy103024@gmail.com>
2026-04-28 18:47:51 +0900

    app

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/database');
-const userRouter = require('./routes/userRouter');
+const passport = require('passport');
+const session = require('express-session');
 
+
+const userRouter = require('./routes/userRouter');
+const authRouter = require('./routes/authRouter');
+const {notFoundHandler, errorHandler} = require('./middlewares/errorMiddleware');
 
 connectDB();
 
@@ -20,7 +25,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended : true}));
 
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/user', userRouter);
+app.use('/Oauth', authRouter);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+
 app.get('/', (req, res) => {
     res.render('index');
 });
