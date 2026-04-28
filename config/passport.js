99e8c437ecdae82089187909540cd102adf59870
yooseunghwan <shy103024@gmail.com>
2026-04-28 18:32:13 +0900

    passport,Oauth

---
+++ config/passport.js
@@ -0,0 +1,97 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const NaverStrategy = require('passport-naver-v2').Strategy;
+const bcrypt = require('bcrypt');
+const userService = require('../services/userService');
+
+passport.use(new localStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, async (email, password, done) => {
+    try {
+        const user = await userService.findUserByEmail(email);
+        if(!user) {
+            return done(null, false, {message: '이메일 또는 비밀번호가 일치하지 않습니다.'});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return done(null, false, {message: '비밀번호가 일치하지 않습니다.'});
+        }
+        return done(null, user);
+    } catch (error) {
+        return done(error);
+    }
+}));
+
+// 구글 로그인
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+}, async (accessToken, refreshToken, profile, done)=>{
+    try {
+        const email = profile.email[0].value;
+
+        let user = await userService.findUserByEmail(email);
+        if(user) {
+            return done(null, user);
+        }
+        const newUser = await userService.createSocialUser(
+            {
+                email,
+                name: profile.displayName,
+                profileImage: profile.photos[0].value,
+                address: '',
+                provider: 'google'
+            }
+        );
+        return done(null, newUser);
+    } catch (error) {
+        return done(error);
+    }
+}));
+
+
+//네이버 로그인
+passport.use(new NaverStrategy({
+    clientID: process.env.NAVER_CLIENT_ID,
+    clientSecret: process.env.NAVER_CLIENT_SECRET,
+    callbackURL: '/auth/naver/callback'
+}, async (accessToken, refreshToken, profile, done)=>{
+    try {
+        const email = profile.email;
+        let user = await userService.findUserByEmail(email);
+        if(user) {
+            return done(null, user);
+        }
+    
+        const newUser = await userService.createSocialUser(
+            {
+                email,
+                name: profile.name,
+                profileImage: profile.profileImage,
+                address: '',
+                provider: 'naver'
+            }
+        );
+        return done(null, newUser);
+    } catch (error) {
+        return done(error);
+    }
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await userService.findUserById(id);
+        done(null, user);
+    } catch (error) {
+        done(error);
+    }
+});
+
+module.exports = passport;
