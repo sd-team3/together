2affc597f3fa46b82490d99c4e6fbace140052ae
JoYeonO <oh2972@gmail.com>
2026-05-14 19:47:53 +0900

    Merge branch 'dev' into dev-separation

---
+++ public/js/deleteConfirm.js
@@ -0,0 +1,11 @@
+function confirmDelete(crewId) {
+    document.getElementById('delete-form').action = '/crew/delete/' + crewId;
+    document.getElementById('modal-delete').classList.add('show');
+}
+
+function deleteModal() {
+    document.getElementById('modal-delete').classList.remove('show');
+}
+
+// 해산이나 탈퇴 누를 때, 팝업창으로 확인하는 스크립트임.
+// 사용시 ejs 상단에 <script src="/js/deleteConfirm.js"></script> 추가 + 본문 아래에 모달용 div 제작
