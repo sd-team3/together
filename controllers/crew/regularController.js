2d9e6e8f5a30b35492c66fb8efad28fb8ab72f47
ShinSeungMin <20211057@daejin.ac.kr>
2026-06-01 20:51:12 +0900

    정기모임 페이지 완
    q

--- controllers/crew/regularController.js
+++ controllers/crew/regularController.js
@@ -85,6 +85,16 @@ const getRegularAPI = async (req, res, next) => {
         next(error);
     }
 };
+// 정기모임 상세 페이지
+const getRegularPage = async (req, res, next) => {
+    try {
+        const { crewId } = req.params;
+        const crew = await regularService.getCrewDetail(crewId);
+        res.render('crew/regular-join-page', { crew });
+    } catch (error) {
+        next(error);
+    }
+}
 
 const getMyCrews = async (req, res) => {
     try {
@@ -164,5 +174,6 @@ module.exports = {
     getCrewDetail,
     postCrewLike,
     getRegular,
-    getRegularAPI
+    getRegularAPI,
+    getRegularPage
 };
