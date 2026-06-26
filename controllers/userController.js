f6012ed93989ecf258132f28f90b5b0ccf09dec5
yooseunghwan <shy103024@gmail.com>
2026-06-26 19:03:07 +0900

    dd

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,7 +67,7 @@ req.login(result, (err) => {
         });
     } catch (error) {
         
-        if (error.code === 11000) {
+        if (error.status === 400) {
             return res.render('user/signup', {
                 errors: {
                     email: error.message
